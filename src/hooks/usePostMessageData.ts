961881ef67ac3c6b405c4e6971766d0d0c19e448
gilmor-port <gilm@port.io>
2026-03-24 16:30:30 +0200

    added post message types

--- src/hooks/usePostMessageData.ts
+++ src/hooks/usePostMessageData.ts
@@ -1,10 +1,49 @@
 import { useEffect, useState } from "react";
 
+export type Page = {
+    identifier: string;
+    pageFilters?: Record<string, unknown>;
+}
+
+export type User = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+    roles: { name: string; orgId: string }[];
+    teams: { name: string; orgId: string }[];
+    type: string;
+    picture: string;
+    status: string;
+    providers: [];
+    companyId: string;
+    companyRole: string;
+}
+
+export type Entity = {
+    identifier: string;
+    title: string;
+    icon: string
+    team: string
+    properties: Record<string, unknown>;
+    relations: Record<string, unknown>;
+}
+
+
+
+export type Params = {
+    [key: string]: {
+        type: string;
+        value: unknown;
+    };
+}
+
 export const usePostMessageData = () => {
-    const [params, setParams] = useState<Record<string, unknown>>({});
-    const [page, setPage] = useState<Record<string, unknown>>({});
-    const [user, setUser] = useState<Record<string, unknown>>({});
-    const [entity, setEntity] = useState<Record<string, unknown>>({});
+    const [params, setParams] = useState<Params>({});
+    const [page, setPage] = useState<Page>();
+    const [user, setUser] = useState<User>();
+    const [entity, setEntity] = useState<Entity>();
     const [portToken, setPortToken] = useState<string | null>(null);
     const [portApiBaseUrl, setPortApiBaseUrl] = useState<string | null>(null);
 
